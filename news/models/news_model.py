d88b2b4669149f09efcc0f8113baa70b19ddd6fb
lucas <lucasrosa223@hotmail.com>
2023-09-27 14:55:49 -0300

    feito ajustes para criacao da migration

--- news/models/news_model.py
+++ news/models/news_model.py
@@ -1,6 +1,6 @@
 from django.db import models
-from models.user_model import Users
-from models.category_model import Categories
+from news.models.user_model import Users
+from news.models.category_model import Categories
 
 
 class News(models.Model):
@@ -8,15 +8,19 @@ class News(models.Model):
     content = models.TextField()
     author = models.ForeignKey(
         Users,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="news"
     )
     created_at = models.DateField()
     image = models.ImageField(
-        upload_to='img/',
+        upload_to='img',
         blank=True,
         null=True
     )
-    categories = models.ManyToManyField(Categories)
+    categories = models.ManyToManyField(
+        Categories,
+        related_name="news"
+        )
 
     def __str__(self):
         return self.title
