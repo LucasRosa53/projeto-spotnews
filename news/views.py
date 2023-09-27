8f52c3fc3b9a656b61391bec2df1a251bdfff73e
lucas <lucasrosa223@hotmail.com>
2023-09-27 18:10:46 -0300

    resolvendo requisito 4

--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import News
 # Create your views here.
+
+
+def req_news(request):
+    news_list = {"news": News.objects.all()}
+    return render(request, "home.html", news_list)
