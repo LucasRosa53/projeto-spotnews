08455a75781e52f04bb152f36b04428c174722d9
lucas <lucasrosa223@hotmail.com>
2023-09-28 16:58:43 -0300

    resolvido conflitos de variaveis, nomes e outros, conflito com req4

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,5 @@ from news.views import req_news, news_details
 
 urlpatterns = [
     path("", req_news, name="home-page"),
-    path("news/int:<id>", news_details, name="news-details-page")
+    path("news/<int:id>", news_details, name="news-details-page"),
 ]
